ec2b636dd73bc20602927f67d1320e67554dd9aa
GGolfz <wisarutgolf11@gmail.com>
2021-02-28 01:43:42 +0700

    [IAMRICH] init: project :sparkles:

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by GGolfz on 28/2/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
