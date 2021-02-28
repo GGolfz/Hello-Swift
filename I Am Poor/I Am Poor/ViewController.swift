1646629b2b203635dbfd0118967583ac13896730
GGolfz <wisarutgolf11@gmail.com>
2021-02-28 20:13:07 +0700

    [IAMPOOR] init: project :sparkles:

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by GGolfz on 28/2/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
