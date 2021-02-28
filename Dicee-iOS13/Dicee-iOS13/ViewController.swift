7d94592e5d9f7ccb3d819d7533039b3e86a90004
GGolfz <wisarutgolf11@gmail.com>
2021-02-28 21:52:33 +0700

    [DICEE] finish: project :sparkles:

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstDice: UIImageView!
+    @IBOutlet weak var secondDice: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func handleButtonTouch(_ sender: Any) {
+        let dice1 = Int.random(in: 0..<6)
+        let dice2 = Int.random(in: 0..<6)
+        firstDice.image = getImage(num: dice1)
+        secondDice.image = getImage(num: dice2)
+    }
+    func getImage(num: Int)  -> UIImage{
+        return [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][num]
+    }
 }
+    
 
