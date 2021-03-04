e08c2da82169fa9083396eba6ffce4f61c7689da
GGolfz <wisarutgolf11@gmail.com>
2021-03-04 09:00:48 +0700

    [XYLOPHONE] finish :sparkles:

--- Xylophone-iOS13/Xylophone/ViewController.swift
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(type: sender.titleLabel?.text ?? "")
+    }
+    func playSound(type: String) {
+        print("Play:", type);
+        let url = Bundle.main.url(forResource: type, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf:url!)
+        player?.play()
+        
+    }
+    func test() -> Int {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    
+    
+    
 }
 
